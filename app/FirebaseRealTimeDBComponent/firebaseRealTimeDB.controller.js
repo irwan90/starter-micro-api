dd0b3b9211ad2cab72b362dbf766374472dfc17c
irwan <irwans.kom1990@gmail.com>
2024-01-22 12:25:25 +0700

    tes

---
+++ app/FirebaseRealTimeDBComponent/firebaseRealTimeDB.controller.js
@@ -0,0 +1,73 @@
+var firebaseRealTimeDBModel = require('./firebaseRealTimeDB.model');
+
+exports.getAllUser = function(req, res) {
+    firebaseRealTimeDBModel.getAllUser(function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.getUserById = function(req, res) {
+    firebaseRealTimeDBModel.getUserById(req.params.userId, function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.addUser = function(req, res) {
+    firebaseRealTimeDBModel.addUser(req.body, function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.updateUser = function(req, res) {
+    firebaseRealTimeDBModel.updateUser(req.body, function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.deleteUserById = function(req, res) {
+    firebaseRealTimeDBModel.deleteUserById(req.params.userId, function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+
+
+exports.getAllProject = function(req, res) {
+    firebaseRealTimeDBModel.getAllProject(function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
+
+exports.getAllProjectByGroup = function(req, res) {
+    firebaseRealTimeDBModel.getAllProjectByGroup(req.params.groupId, function(error, result) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+};
---
+++ routes/firebaseRealTimeDB.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var firebaseSettings = require('../config/firebaseConfig');
+
+var firebaseRealTimeDBValidator = require('../app/FirebaseRealTimeDBComponent/firebaseRealTimeDB.validator');
+
+// get all user
+router.get('/getAllUser', firebaseRealTimeDBValidator.getAllUser);
+
+// get user by id - (params: [userId]) - (userId is required)
+router.get('/getUserById/:userId', firebaseRealTimeDBValidator.getUserById);
+
+// add new user - (body: [userId,name,address,email,mobileNo]) - (name,address,email,mobileNo is required)
+router.post('/addUser', firebaseRealTimeDBValidator.addUser);
+
+// update user - (body: [userId,name,address,email,mobileNo]) - (name,address,email,mobileNo is required)
+router.put('/updateUser', firebaseRealTimeDBValidator.updateUser);
+
+// delete user by id - (params: [userId]) - (userId is required)
+router.delete('/deleteUserById/:userId', firebaseRealTimeDBValidator.deleteUserById);
+
+router.get('/getAllProject', firebaseRealTimeDBValidator.getAllProject);
+router.get('/getAllProjectByGroup/:groupId', firebaseRealTimeDBValidator.getAllProjectByGroup);
+
+module.exports = router;
